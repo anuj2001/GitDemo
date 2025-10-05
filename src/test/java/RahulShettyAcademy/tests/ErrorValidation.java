7a2a00127ebbbbc2ea4b4e504a0f73b5993b7fb4
anuj2001 <agupta14_be20@thapar.edu>
2025-10-05 15:17:30 +0530

    American Architect

--- src/test/java/RahulShettyAcademy/tests/ErrorValidation.java
+++ src/test/java/RahulShettyAcademy/tests/ErrorValidation.java
@@ -37,7 +37,7 @@ public class ErrorValidation extends BaseTest {
 			ProductCatalogue productCatalogue = landingPage.loginApplication("agupta14_be20@thapar.edu", "@Neheart1");
 			List<WebElement> products=productCatalogue.getProductsList();
 			WebElement prod=productCatalogue.getProductByName(productName);
-			productCatalogue.addProductToCart("ZARA COAT 420");
+			productCatalogue.addProductToCart("ZARA COAT 520");
 			CartPage cartPage = productCatalogue.goToCartPage();
 			Boolean match=cartPage.verifyProductDisplay(productName);
 			Assert.assertTrue(match);			
